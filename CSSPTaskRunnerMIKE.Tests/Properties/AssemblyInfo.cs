f166a13e3b99610a2de852bd016ae7d30cae367d
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-08-16 17:13:38 -0300

    Initial commit

---
+++ CSSPTaskRunnerMIKE.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSSPTaskRunnerMIKE.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Environment Canada")]
+[assembly: AssemblyProduct("CSSPTaskRunnerMIKE.Tests")]
+[assembly: AssemblyCopyright("Copyright © Environment Canada 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("278944af-17ae-4bee-a0c7-ed28d7295f48")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSSPTaskRunnerMIKEHelper.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSSPTaskRunnerMIKEHelper.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Environment Canada")]
+[assembly: AssemblyProduct("CSSPTaskRunnerMIKEHelper.Tests")]
+[assembly: AssemblyCopyright("Copyright © Environment Canada 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("338dff28-b932-4c3f-8ae9-a15df16b15be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
